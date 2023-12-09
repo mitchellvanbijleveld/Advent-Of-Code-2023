f86aad66a7f7fcd8771b6105249cefb3c841ce14
mitchellvanbijleveld <github.com@mitchellvanbijleveld.email>
2023-12-09 01:01:29 +0100

    Create basically empty script for day 2

---
+++ Day_02.py
@@ -0,0 +1,7 @@
+import os
+
+FilePath_InputFile = os.path.abspath('Advent-Of-Code-2023/Input/Day_02.txt')
+
+with open(FilePath_InputFile, 'r') as InputFile:
+    for Line in InputFile:
+        print(Line)
